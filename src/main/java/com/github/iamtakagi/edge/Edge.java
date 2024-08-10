261153dbc2b35e1e106a7b0b8017f4fbabbb14a6
iamtakagi <github@iamtakagi.net>
2024-08-10 21:55:48 +0900

    remove: TargetEntityDistanceBar

--- src/main/java/com/github/iamtakagi/edge/Edge.java
+++ src/main/java/com/github/iamtakagi/edge/Edge.java
@@ -3,10 +3,8 @@ package com.github.iamtakagi.edge;
 import java.text.SimpleDateFormat;
 
 import org.bukkit.Bukkit;
-import org.bukkit.WeatherType;
 import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,12 +12,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.Vector;
 
 import net.kyori.adventure.text.Component;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.megavex.scoreboardlibrary.api.ScoreboardLibrary;
 import net.megavex.scoreboardlibrary.api.exception.NoPacketAdapterAvailableException;
 import net.megavex.scoreboardlibrary.api.noop.NoopScoreboardLibrary;
@@ -43,7 +38,6 @@ public class Edge extends JavaPlugin {
     this.loadConfig();
     this.setupScoreboard();
     this.getServer().getPluginManager().registerEvents(new SidebarListener(), this);
-    this.getServer().getScheduler().runTaskTimer(instance, new TargetEntityDistanceActionbarTask(), 0, 1);
   }
 
   @Override
@@ -189,41 +183,15 @@ public class Edge extends JavaPlugin {
     }
   }
 
-  class TargetEntityDistanceActionbarTask implements Runnable {
-
-    @Override
-    public void run() {
-      for (Player player : getServer().getOnlinePlayers()) {
-        Entity target = Utils.getTargetEntity(player);
-
-        if (target == null) {
-          return;
-        }
-
-        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, player.getUniqueId(),
-            new TextComponent(ChatColor.translateAlternateColorCodes('&',
-                config.getActionbarSettings().getTargetEntityDistanceSettings().getFormat()
-                    .replace("{TARGET}", target.getType().getEntityClass().getSimpleName()).replace("{DISTANCE}",
-                        "" + (Math.floor(player.getLocation().distance(target.getLocation()) * 100)) / 100))));
-      }
-    }
-  }
-
   class EdgeConfig {
     private SidebarSettings sidebarSettings;
-    private ActionbarSettings actionbarSettings;
 
     EdgeConfig(YamlConfiguration yaml) {
       this.sidebarSettings = new SidebarSettings(yaml);
-      this.actionbarSettings = new ActionbarSettings(yaml);
     }
 
     public SidebarSettings getSidebarSettings() {
       return this.sidebarSettings;
-    }
-
-    public ActionbarSettings getActionbarSettings() {
-      return this.actionbarSettings;
     }
 
     class SidebarSettings {
@@ -279,36 +247,6 @@ public class Edge extends JavaPlugin {
         }
       }
     }
-
-    class ActionbarSettings {
-      private TargetEntityDistanceSettings targetEntityDistanceSettings;
-
-      ActionbarSettings(YamlConfiguration yaml) {
-        this.targetEntityDistanceSettings = new TargetEntityDistanceSettings(yaml);
-      }
-
-      public TargetEntityDistanceSettings getTargetEntityDistanceSettings() {
-        return this.targetEntityDistanceSettings;
-      }
-
-      class TargetEntityDistanceSettings {
-        private boolean isEnabled;
-        private String format;
-
-        TargetEntityDistanceSettings(YamlConfiguration yaml) {
-          this.isEnabled = yaml.getBoolean("actionbar.target_entity_distance.enabled");
-          this.format = yaml.getString("actionbar.target_entity_distance.format");
-        }
-
-        public boolean isEnabled() {
-          return this.isEnabled;
-        }
-
-        public String getFormat() {
-          return this.format;
-        }
-      }
-    }
   }
 }
 
@@ -357,37 +295,6 @@ class Utils {
       e.printStackTrace();
     }
     return recentTps;
-  }
-
-  public static Player getTargetPlayer(final Player player) {
-    return getTarget(player, player.getWorld().getPlayers());
-  }
-
-  public static Entity getTargetEntity(final Entity entity) {
-    return getTarget(entity, entity.getWorld().getEntities());
-  }
-
-  public static <T extends Entity> T getTarget(final Entity entity,
-      final Iterable<T> entities) {
-    if (entity == null)
-      return null;
-    T target = null;
-    final double threshold = 1;
-    for (final T other : entities) {
-      final Vector n = other.getLocation().toVector()
-          .subtract(entity.getLocation().toVector());
-      if (entity.getLocation().getDirection().normalize().crossProduct(n)
-          .lengthSquared() < threshold
-          && n.normalize().dot(
-              entity.getLocation().getDirection().normalize()) >= 0) {
-        if (target == null
-            || target.getLocation().distanceSquared(
-                entity.getLocation()) > other.getLocation()
-                    .distanceSquared(entity.getLocation()))
-          target = other;
-      }
-    }
-    return target;
   }
 
   public static String getWorldWeather(World world) {
